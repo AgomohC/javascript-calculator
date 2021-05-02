7684b707667bdf3d4511b37d8dfa10fa693161bf
AgomohC <chinaemerem98@yahoo.com>
2021-05-02 19:16:04 +0100

    added components folder

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import React from "react";
 
 const App = () => {
   return (
-    <main className="bg-secondary container">
-      <div className="row d-flex align-items-center">
-        <section className="col mx-auto"></section>
+    <main className="bg-secondary">
+      <div className="container">
+        <div className="row d-flex height align-items-center">
+          <section className="col-sm-9 col-md-6 col-lg-4 mx-auto bg-light">
+            <article>
+              <input type="text" className="mx-auto" />
+            </article>
+            <article></article>
+          </section>
+        </div>
       </div>
     </main>
   );
