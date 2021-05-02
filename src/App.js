9dd9920520910c81b92c90f03fdd0b60caca9eda
AgomohC <chinaemerem98@yahoo.com>
2021-05-02 20:37:10 +0100

    finished styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
+import Buttons from "./components/Buttons";
+import Display from "./components/Display";
 
 const App = () => {
   return (
     <main className="bg-secondary">
       <div className="container">
         <div className="row d-flex height align-items-center">
-          <section className="col-sm-9 col-md-6 col-lg-4 mx-auto bg-light">
-            <article>
-              <input type="text" className="mx-auto" />
-            </article>
-            <article></article>
+          <section className="col-9 col-sm-9 col-md-6 col-lg-4 mx-auto bg-light height-52">
+            <h5 className="text-center text-capitalize text-secondary mt-4">
+              Javascript Calculator
+            </h5>
+            <Display></Display>
+            <Buttons></Buttons>
           </section>
         </div>
       </div>
---
+++ src/components/display.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Display = () => {
+  return (
+    <article>
+      <input
+        type="text"
+        className="mx-auto form-control form-control-lg mt-3 bg-secondary text-light"
+      />
+    </article>
+  );
+};
+
+export default Display;
